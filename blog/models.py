23896884d4e6da5bc434152e090e2180eb2f8a4a
Oscar Bäckman <jan.oscar.backman@gmail.com>
2024-08-05 12:52:44 +0000

    changed faulty code in comments

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=CASCADE, related_name="commenter")
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
