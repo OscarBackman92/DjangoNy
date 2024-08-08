11b03bc4d5cf27b348427e684c6557cf7687a473
Oscar Bäckman <jan.oscar.backman@gmail.com>
2024-08-08 20:04:00 +0000

    added crispyforms, forms.py in blog,Comments in views.py/blog and settings, Boostrap in settings.py codestar

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
+
 # Create your views here.
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = 'blog/index.html'
+    template_name = "blog/index.html"
     paginate_by = 6
+
 
 def post_detail(request, slug):
     """
@@ -15,18 +20,29 @@ def post_detail(request, slug):
     **Context**
 
     ``post``
-        An instance of :model: `blog.Post`.
+        An instance of :model:`blog.Post`.
 
-    **Template**
+    **Template:**
 
-    :demplate:`blog/post_detail.html`
+    :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -35,5 +51,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form
         },
     )
